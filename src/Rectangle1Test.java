1a2ca72893afc51c83af9f159de458631dedd9fd
Michael Liu <michael.liu95@gmail.com>
2016-01-31 17:47:15 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/Rectangle1Test.java changed at Sun Jan 31 17:47:15 EST 2016

--- src/Rectangle1Test.java
+++ src/Rectangle1Test.java
@@ -1,49 +1,32 @@
-/**
- *
- */
-//package Tests;
-
-import java.lang.reflect.Method;
 import student.TestCase;
-import student.testingsupport.annotations.Hint;
-import student.testingsupport.annotations.ScoringWeight;
-
-import static student.testingsupport.ReflectionSupport.*;
 
 /**
  * @author ayaan
- *
+ * @version 2
  */
-public class Rectangle1Test extends TestCase {
-	private Class<?> Rectangle1;
-	private Method main;
+public class Rectangle1Test
+    extends TestCase
+{
 
-	public void setUp() {
+    /**
+     * This method sets up the tests that follow.
+     */
+    public void setUp()
+    {
+        // Nothing here yet
+    }
 
-	}
 
-	@Hint("Please check the format and syntax of your output.")
-	@ScoringWeight(0.0)
-	public void testRectangle1() {
-		try {
-			this.Rectangle1 = reloadClassForName("Rectangle1");
-		} catch (Throwable e) {
-			if (e instanceof Error) {
-				throw e;
-			} else {
-				throw new RuntimeException(e);
-			}
-		}
+    // ----------------------------------------------------------
+    /**
+     * This method is simply to get code coverage of the class declaration.
+     */
+    public void testRInit()
+    {
+        Rectangle1 dum = new Rectangle1();
+        assertNotNull(dum);
+        Rectangle1.main(null);
+        assertFuzzyEquals("Hello, World\n", systemOut().getHistory());
+    }
 
-		this.main = getMethod(this.Rectangle1, "main", String[].class);
-
-		String[] params = { "SyntaxTest.txt" };
-		Object[] args = { params };
-
-		try {
-			invokeEx(null, this.main, args[0]);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 }
