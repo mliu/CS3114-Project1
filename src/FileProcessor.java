6bf75df1c1919732785f5c377d7c687d02b66b9d
Michael Liu <michael.liu95@gmail.com>
2016-01-31 18:41:46 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/FileProcessor.java changed at Sun Jan 31 18:41:46 EST 2016

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -12,16 +12,53 @@ public class FileProcessor {
 	
 	}
 
-	public void insert(){
+	public void parseInsert(String[] args){
 	    
 	}
 	
-	public void regionsearch(String args) {
+	public void parseRegionsearch(String[] args) {
 	    
 	}
 	
+	public void parseRemove(String[] args) {
+	    
+	}
+    
+    public void parseSearch(String[] args) {
+        
+    }
+    
+    public void parseIntersections() {
+        
+    }
+    
+    public void parseDump() {
+        
+    }
+    
 	public void parseString(String str) {
 	    String plusSpaceDelim = "[ +]+";
         String[] tokens = str.split(plusSpaceDelim);
+        
+        switch(tokens[0]) {
+            case "insert":
+                parseInsert(tokens);
+                break;
+            case "remove":
+                parseRemove(tokens);
+                break;
+            case "regionsearch":
+                parseRegionsearch(tokens);
+                break;
+            case "search":
+                parseSearch(tokens);
+                break;
+            case "intersections":
+                parseIntersections();
+                break;
+            case "dump":
+                parseDump();
+                break;
+        }
 	}
 }
