e79ccbbc868b493a02d8b6bbf9d999c505506edc
Michael Liu <michael.liu95@gmail.com>
2016-01-31 19:05:11 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/FileProcessor.java changed at Sun Jan 31 19:05:11 EST 2016

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -9,70 +9,70 @@
  */
 public class FileProcessor {
 
-	public FileProcessor() {
-	
-	}
+    public FileProcessor() {
 
-	public void parseInsert(String[] args){
-	    System.out.print("Rectangle inserted: (");
-	    
-	    for (int i = 1; i < args.length; i++) {
-	        System.out.print(args[i]);
-	        if (i != args.length - 1) {
-	            System.out.print(", ");
-	        }
-	    }
-	    System.out.println(")");
-	}
-	
-	public void parseRegionsearch(String[] args) {
-	    System.out.println("Regionsearch results: ");
-	}
-	
-	public void parseRemove(String[] args) {
-	    if (args.length == 2) {
-	        System.out.println("Rectangle removed: ");
-	    }
-	    else {
-	        System.out.println("Rectangle removed: ");
-	    }
-	}
-    
+    }
+
+    public void parseInsert(String[] args){
+        System.out.print("Rectangle inserted: (");
+
+        for (int i = 1; i < args.length; i++) {
+            System.out.print(args[i]);
+            if (i != args.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(")");
+    }
+
+    public void parseRegionsearch(String[] args) {
+        System.out.println("Regionsearch results: ");
+    }
+
+    public void parseRemove(String[] args) {
+        if (args.length == 2) {
+            System.out.println("Rectangle removed: ");
+        }
+        else {
+            System.out.println("Rectangle removed: ");
+        }
+    }
+
     public void parseSearch(String[] args) {
         System.out.println("Rectangles found: ");
     }
-    
+
     public void parseIntersections() {
         System.out.println("Intersection pairs: ");
     }
-    
+
     public void parseDump() {
         System.out.println("SkipList dump: ");
     }
-    
-	public void parseString(String str) {
-	    String plusSpaceDelim = "[ +]+";
+
+    public void parseString(String str) {
+        String plusSpaceDelim = "[ +]+";
         String[] tokens = str.split(plusSpaceDelim);
-        
+
         switch(tokens[0]) {
-            case "insert":
-                parseInsert(tokens);
-                break;
-            case "remove":
-                parseRemove(tokens);
-                break;
-            case "regionsearch":
-                parseRegionsearch(tokens);
-                break;
-            case "search":
-                parseSearch(tokens);
-                break;
-            case "intersections":
-                parseIntersections();
-                break;
-            case "dump":
-                parseDump();
-                break;
+        case "insert":
+            parseInsert(tokens);
+            break;
+        case "remove":
+            parseRemove(tokens);
+            break;
+        case "regionsearch":
+            parseRegionsearch(tokens);
+            break;
+        case "search":
+            parseSearch(tokens);
+            break;
+        case "intersections":
+            parseIntersections();
+            break;
+        case "dump":
+            parseDump();
+            break;
         }
-	}
+    }
 }
