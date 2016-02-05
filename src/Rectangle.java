fe93b572a3986b2a638b69745035935ec93638c3
Michael Liu <michael.liu95@gmail.com>
2016-02-05 14:49:03 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/Rectangle.java changed at Fri Feb 05 14:49:02 EST 2016

---
+++ src/Rectangle.java
@@ -0,0 +1,13 @@
+
+public class Rectangle {
+
+    public int x, y, width, height;
+    
+    public Rectangle(int xPos, int yPos, int w, int h) {
+        x = xPos;
+        y = yPos;
+        width = w;
+        height = h;
+    }
+
+}
