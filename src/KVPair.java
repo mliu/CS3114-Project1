dd5f3160edcedc172aff612d13469d82e99991cf
Michael Liu <michael.liu95@gmail.com>
2016-02-05 14:52:45 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/KVPair.java changed at Fri Feb 05 14:52:44 EST 2016

--- src/KVPair.java
+++ src/KVPair.java
@@ -1,4 +1,15 @@
 
 public class KVPair<String extends Comparable<String>, Rectangle> implements Comparable<KVPair<String, Rectangle>> {
 
+    private String key;
+    private Rectangle value;
+    
+    public KVPair(String newKey, Rectangle newValue) {
+        key = newKey;
+        value = newValue;
+    }
+    
+    public int compareTo(KVPair<String, Rectangle> it) {
+        return strcmp(key, it.key());
+    }
 }
