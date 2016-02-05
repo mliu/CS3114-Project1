dc2e9e9dd8d74aa84f5626cdef58b18f065e60d7
Michael Liu <michael.liu95@gmail.com>
2016-02-05 15:48:57 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/SkipNode.java changed at Fri Feb 05 15:48:57 EST 2016

--- src/SkipNode.java
+++ src/SkipNode.java
@@ -1,4 +1,11 @@
 
 public class SkipNode<E> {
 
+    private E key;
+    private SkipNode forward;
+    
+    public SkipNode(E newKey, SkipNode pointer) {
+        key = newKey;
+        forward = pointer;
+    }
 }
