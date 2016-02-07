427ecb433d1d87200789cdff7b15800e37cd8c0f
Michael Liu <michael.liu95@gmail.com>
2016-02-06 19:35:08 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/SkipList.java changed at Sat Feb 06 19:35:08 EST 2016

--- src/SkipList.java
+++ src/SkipList.java
@@ -1,4 +1,14 @@
+import java.util.Random;
 
 public class SkipList {
-
+    
+    private Random rnd = new Random();
+    
+    private int randomLevel() {
+        int lev;
+        for (lev = 0; rnd.nextInt(2) == 0; lev++);
+        return lev;
+    }
+    
+    
 }
