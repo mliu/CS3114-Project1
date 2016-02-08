c24d5474cfb793b458d1ae9afe71ecc5557186b5
Michael Liu <michael.liu95@gmail.com>
2016-02-08 15:23:54 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/Rectangle.java changed at Mon Feb 08 15:23:54 EST 2016

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -25,7 +25,7 @@ public class Rectangle {
     }
     
     public boolean intersects(Rectangle rect) {
-        return (rect.x + rect.width) < x || (x + w) < rect.x || (rect.y + rect.height) < y || (y + h) < rect.y )
+        return (rect.x + rect.width) < x || (x + width) < rect.x || (rect.y + rect.height) < y || (y + height) < rect.y );
     }
 
 }
