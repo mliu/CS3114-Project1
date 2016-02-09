4365ca63f964e48f7cc40328172689174a740fb6
Michael Liu <michael.liu95@gmail.com>
2016-02-08 21:02:30 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/FileProcessor.java changed at Mon Feb 08 21:02:29 EST 2016

--- src/FileProcessor.java
+++ src/FileProcessor.java
@@ -58,7 +58,7 @@ public class FileProcessor {
         Rectangle rect = new Rectangle(name, x, y, w, h);
         KVPair<String, Rectangle> pair = new KVPair<String, Rectangle>(name, rect);
         if (list.insert(pair)) {
-            System.out.print("Rectangle inserted: (");
+            System.out.print("Rectangle inserted: ");
             this.printOut(args);
         }
     }
