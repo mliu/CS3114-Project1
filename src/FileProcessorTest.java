b083c06cd48625b821767c607b570f3a37c71771
Michael Liu <michael.liu95@gmail.com>
2016-02-08 22:31:20 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/FileProcessorTest.java changed at Mon Feb 08 22:31:20 EST 2016

--- src/FileProcessorTest.java
+++ src/FileProcessorTest.java
@@ -11,14 +11,16 @@ public class FileProcessorTest extends TestCase {
     private FileProcessor processor;
     
     /*
-     *  Set up the test case
+     * Set up the test case
      */
     public void setUp() {
         processor = new FileProcessor();
     }
     
-    // Test the file processor doesn't print anything
-    // for an invalid command
+    /**
+     * Tests that the file processor doesn't print anything
+     * for an invalid command
+     */
     public void testWrongCommand() {
         processor.parseString("peanuts 7");
         assertFuzzyEquals("", systemOut().getHistory());
