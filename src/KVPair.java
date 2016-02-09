d88567b53906d19343ca9600eec1ff4662c1c03e
Michael Liu <michael.liu95@gmail.com>
2016-02-08 21:55:23 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/KVPair.java changed at Mon Feb 08 21:55:23 EST 2016

--- src/KVPair.java
+++ src/KVPair.java
@@ -5,7 +5,8 @@
  * @author mliu95
  * @version 1
  */
-public class KVPair<K extends Comparable<K>, E> implements Comparable<KVPair<K,E>> {
+public class KVPair<K extends Comparable<K>, E> 
+    implements Comparable<KVPair<K,E>> {
 
     private K key;
     private E value;
