e4bb7b20a1d2d34fe47e6e7a1f51a80120316777
Michael Liu <michael.liu95@gmail.com>
2016-02-08 21:52:52 -0500

    File /C:/Users/Michael/VT/Junior/CS 3114/Project 1/Assignment1/src/RectangleTest.java changed at Mon Feb 08 21:52:52 EST 2016

---
+++ src/RectangleTest.java
@@ -0,0 +1,22 @@
+import student.TestCase;
+
+/**
+ * @author jordanrw
+ * @author mliu95
+ * @version 1
+ */
+
+public class RectangleTest extends TestCase {
+    
+    private Rectangle rect;
+    
+    public void setUp() {
+        rect = new Rectangle("test", 1, 1, 5, 5);
+    }
+    
+    public void testEquals() {
+        Object blah = new Object();
+        assertFalse
+    }
+
+}
